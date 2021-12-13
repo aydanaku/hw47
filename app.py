6d2480652ce540860494110db3b5e26649c8f6df
Aydana <68425611+aydanaku@users.noreply.github.com>
2021-12-13 21:46:37 +0300

    added adding function

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-# @app.route('/<test>/') #принимает переменную вместо <test>
-# def homepage(test):
-#     # return 'Hello world!'
-#     test = test.upper()
-#     return render_template('index.html', my_var=test)
-
-# @app.route('/')
-# def homepage():
-#     f = open('goods.txt', 'r', encoding='utf-8')
-#     txt = f.readlines()
-#     return render_template('index.html', goods=txt)
-
 @app.route('/')
 def homepage():
-    f = open('goods.xlsx', 'r', encoding='utf-8')
+    f = open('goods.txt', 'r', encoding='utf-8')
     txt = f.readlines()
     return render_template('index.html', goods=txt)
+
+
+@app.route('/add/', methods=["POST"])
+def add():
+    good = request.form['good']
+    f = open('goods.txt', "a+", encoding='utf-8')
+    f.write(good + "\n")
+    f.close()
+    return """
+        <h1>Инвентарь пополнен</h1>
+        <a href='/'>Домой</a>
+    """
